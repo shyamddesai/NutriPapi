517ab3801fca4736eeeb26f057cce369cefa5620
MartinEskaros <martineskaros@gmail.com>
2024-02-28 21:24:16 -0500

    Fixed URLs for both app and project

--- Backend/NutriPapiApp/urls.py
+++ Backend/NutriPapiApp/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import signup_view, signin_view, logged_in_view, user_info_view, add_ingredients_to_fridge_view, update_user_info_view
+from .views import signup_view, signin_view, logged_in_view, user_info_view, add_ingredients_to_fridge_view
 
 urlpatterns = [
     path('signup/', signup_view, name='signup'),
@@ -7,5 +7,4 @@ urlpatterns = [
     path('loggedin/', logged_in_view, name='loggedin'), 
     path('user/info/', user_info_view, name='user_info'),
     path('fridge/add_ingredients/', add_ingredients_to_fridge_view, name='add_ingredients_to_fridge'),
-    path('user/update_info/', update_user_info_view, name='update_user_info'),
 ]
