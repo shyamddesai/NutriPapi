4ad9b51debaee02f7b9e40f21994fe0cb3072d11
MasaKagami <97225192+MasaKagami@users.noreply.github.com>
2024-03-25 17:37:29 -0400

    another1

--- reactfront/src/pages/Hub.js
+++ reactfront/src/pages/Hub.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import Hub from '../components/Hub/hub'
 import HubNav from '../components/HubNav/hubNav'
-// import Nav from '../components/Login/NavBar/loginNavbar'
-import Nav from '../components/NavBar/navbar'
+import Nav from '../components/Login/NavBar/loginNavbar'
+// import Nav from '../components/NavBar/navbar'
 
 function hub(){
     return(
