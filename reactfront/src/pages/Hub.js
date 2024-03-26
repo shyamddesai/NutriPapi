a48c068b30196fc49ba3c7e3142f5d80714f680f
MasaKagami <97225192+MasaKagami@users.noreply.github.com>
2024-03-26 09:19:41 -0400

    fixed conflict issue

--- reactfront/src/pages/Hub.js
+++ reactfront/src/pages/Hub.js
@@ -2,7 +2,6 @@ import React from 'react'
 import Hub from '../components/Hub/hub'
 import HubNav from '../components/HubNav/hubNav'
 import Nav from '../components/Login/NavBar/loginNavbar'
-// import Nav from '../components/NavBar/navbar'
 
 function hub(){
     return(
