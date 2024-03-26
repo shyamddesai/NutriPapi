c560ee4101c8e0b950ec9427fafbc5c7ded17358
MasaKagami <97225192+MasaKagami@users.noreply.github.com>
2024-03-25 21:11:48 -0400

    finished hub

--- reactfront/src/components/HubNav/hubNav.js
+++ reactfront/src/components/HubNav/hubNav.js
@@ -5,21 +5,10 @@ const hubnav = () => {
   return (
     <nav className='hubnav'>
         <div className='hubnavheader'>
-          <div className='hubheaderHome'>
-            Home
-          </div>
-          <div className='hubheaderMeals'>
-            Meals
-          </div>
-          <div className='hubheaderProfile'>
-            Profile
-          </div>
-          <div className='hubheaderSettings'>
-            Settings
-          </div>
-          <div className='hubheader'>
-            
-          </div>
+          <a href="/hub" className="hubnavTab">Home</a>
+          <a href="/hub/meals" className="hubnavTab">Meals</a>
+          <a href="/hub/profile" className="hubnavTab">Profile</a>
+          <a href="/hub/settings" className="hubnavTab">Settings</a>       
         </div>
     </nav>
   )
